ff84cc5f7af40cea885d355d2b50add58ddd7b19
Darren Mason <djmason9@gmail.com>
2023-10-12 06:45:19 -0700

    Update gif_to_array.py

--- gif_to_array.py
+++ gif_to_array.py
@@ -1,30 +1,31 @@
 from PIL import Image
 
-imageObject = Image.open("[YOUR_GIF_PATH_HERE]")
+fileName = "[FIILE]"
+pathName= "[/PATH/]" + fileName + ".gif"
 
-# Get the dimensions of the resized frame (320x170)
-frame_width, frame_height = [YOUR_WIDTH], [YOUR_HEIGHT]
+imageObject = Image.open(pathName)
+
 
 # Calculate the size of each frame's data array
 frame_size = frame_width * frame_height
 
-print(imageObject.is_animated)
-print(imageObject.n_frames)
 frames_data = []
 # Display individual frames from the loaded animated GIF file
 for frame in range(0, imageObject.n_frames):
     imageObject.seek(frame)
     rgb_pixels = list(imageObject.convert("RGB").getdata())
+    # print(rgb_pixels)
 
     frame_data = [((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3) for r, g, b in rgb_pixels]
     frames_data.append(frame_data)
 
 # Save the array to a file named "output_array.txt"
-output_filename = "[YOUR_FILE_NAME_HERE]"
+output_filename = fileName + ".h"
 
 with open(output_filename, "w") as output_file:
-    output_file.write(f"#define PROGMEM\n\nint framesNumber = {len(frames_data)}; int aniWidth = {frame_width}; int aniHeight = {frame_height};\n\n")
-    output_file.write("const unsigned short logo2[][{}] PROGMEM = {{\n".format(frame_size))
+    output_file.write(f"#define PROGMEM\n\nint {fileName}framesNumber = {len(frames_data)}; int aniWidth = {frame_width}; int aniHeight = {frame_height};\n\n")
+    output_file.write(f"const unsigned short {fileName}")
+    output_file.write("[][{}] PROGMEM = {{\n".format(frame_size))
     for idx, frame_data in enumerate(frames_data):
         comma = "," if idx < len(frames_data) - 1 else ""
         output_file.write("    {" + ",".join([f"0x{val:04X}" for val in frame_data]) + f"}}{comma}\n")
