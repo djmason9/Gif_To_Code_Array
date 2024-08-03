e932d25adf9e256c6522f203f65fc9c24d5d9e9e
DJ Mason <djmason9@gmail.com>
2024-08-03 10:06:25 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,14 +1,17 @@
-
 import subprocess
 
 video_file = "/home/pi/Desktop/videos/80s.mp4"
 
-command = ["mpv",
-"--monitoraspect=4:3",
-"--fs",
-"--loop-file=inf",
-"--video-unscaled=yes",
-"--audio=auto",
-video_file]
+command = [
+    "mpv",
+    "--monitoraspect=4:3",
+    "--fs",
+    "--loop-file=inf",
+    "--video-unscaled=yes",
+    "--audio=auto",
+    "--audio-device=pulse/bluez_sink.08_EB_ED_F3_8C_1B.a2dp_sink",
+    "--volume=20",
+    video_file
+]
 
 subprocess.run(command)
